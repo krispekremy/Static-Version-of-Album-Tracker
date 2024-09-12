3c857fc2bfb8809e2afeb8f688b9aa9dbee1e6ba
MasonCaldwell <krispekremy@gmail.com>
2024-09-12 14:34:09 -0700

    updating comments

--- src/components/GoatAlbums.tsx
+++ src/components/GoatAlbums.tsx
@@ -1,12 +1,49 @@
+import { Album } from "../types";
 import AlbumInput from "./AlbumInput";
-import GoatAlbumsList from "./GoatAlbumsList";
+import vSnaresImage from "../assets/vsnares.jpg";
+import { useState } from "react";
+import AlbumList from "./AlbumList";
 // This code is set up to be the far right column that is a list for my favorite albums of all time. It calls the AlbumInput component and the GoatAlbumsList component.
+
+const GOAT_TEST_ALBUMS: Album[] = [
+  //I've typed the variable goatAlbumsList as being an array containing the Album type.
+  {
+    id: 0,
+    artist: "Venetian Snares",
+    name: "Rossz Csillag Alatt Született",
+    image: vSnaresImage,
+    link: "https://venetiansnares.bandcamp.com/album/rossz-csillag-alatt-sz-letett",
+    list: 2,
+  },
+];
+
 export default function GoatAlbums() {
+  const [goatAlbumList, setGoatAlbumList] = useState(GOAT_TEST_ALBUMS);
+
+  const addNewAlbum = () => {
+    const newAlbum = {
+      id: goatAlbumList.length
+        ? goatAlbumList[goatAlbumList.length - 1].id + 1
+        : 0,
+      artist: "Venetian Snares",
+      name: "Rossz Csillag Alatt Született",
+      image: vSnaresImage,
+      link: "https://venetiansnares.bandcamp.com/album/rossz-csillag-alatt-sz-letett",
+      list: 2,
+    };
+
+    setGoatAlbumList([...goatAlbumList, newAlbum]);
+  };
+
+  const deleteAlbum = (idToDelete: number) => {
+    setGoatAlbumList(goatAlbumList.filter((album) => album.id !== idToDelete));
+  };
+
   return (
     <>
       <h4>Greatest of All Time</h4>
-      <AlbumInput />
-      <GoatAlbumsList />
+      <AlbumInput addNewAlbum={addNewAlbum} />
+      <AlbumList albumList={goatAlbumList} deleteAlbum={deleteAlbum} />
     </>
   );
 }
--- src/components/NewAlbums.tsx
+++ src/components/NewAlbums.tsx
@@ -4,7 +4,8 @@ import AlbumList from "./AlbumList";
 import heavenImage from "../assets/heaven.jpg";
 import { useState } from "react";
 
-const newAlbums: Album[] = [
+//this is my test data to start the state with so it doesn't start empty
+const NEW_ALBUM_TEST_DATA: Album[] = [
   {
     id: 0,
     artist: "HEAVEN THROUGH VIOLENCE",
@@ -14,11 +15,12 @@ const newAlbums: Album[] = [
     list: 0,
   },
 ];
-
+//now we're exporting the main function of newalbums.
 export default function NewAlbums() {
-  const [newAlbumList, setNewAlbumList] = useState(newAlbums);
+  const [newAlbumList, setNewAlbumList] = useState(NEW_ALBUM_TEST_DATA); // setting the state!
 
   const addNewAlbum = () => {
+    //this function lets us add a new album to the newalbum list!  it uses the same data every time for now!
     const newAlbum = {
       id: newAlbumList.length
         ? newAlbumList[newAlbumList.length - 1].id + 1
@@ -30,18 +32,20 @@ export default function NewAlbums() {
       list: 0,
     };
 
-    setNewAlbumList([...newAlbumList, newAlbum]);
+    setNewAlbumList([...newAlbumList, newAlbum]); //now we're calling our set state function to be set to a copy of the current state plus the new album!
   };
 
   const deleteAlbum = (idToDelete: number) => {
     setNewAlbumList(newAlbumList.filter((album) => album.id !== idToDelete));
-  };
+  }; // this function lets us delete an album!
 
   return (
     <>
       <h4>Albums I Wanna Listen To</h4>
-      <AlbumInput addNewAlbum={addNewAlbum} />
-      <AlbumList albumList={newAlbumList} deleteAlbum={deleteAlbum} />
+      <AlbumInput addNewAlbum={addNewAlbum} />{" "}
+      {/*Here I'm passing the addNewAlbum function down to the album input component inside the newalbums component through a prop called addNewAlbum! */}
+      <AlbumList albumList={newAlbumList} deleteAlbum={deleteAlbum} />{" "}
+      {/* Here I'm passing the newAlbumList which is our state through the to the albumlist component through the albumlist prop that I've made, as well as the deletealbum function through the deletalbum prop! That delete album prop will then get passed down through to the album card component where it will get attached to a button in the albumcard! */}
     </>
   );
 }
